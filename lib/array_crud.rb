9cfd6e36ed3b491b922f1fa35dc44deaeb7cb248
Haley Ferro <haleyvferro@gmail.com>
2020-06-14 23:48:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,8 +28,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  ["I", "am", "really", "learning"]
-  array [0]
+  ["wow", "I", "am", "really", "learning", "arrays!"]
+  array [2]
 end
 
 def retrieve_first_element_from_array(array)
